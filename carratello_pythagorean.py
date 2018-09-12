8e2b9ac3060d40bdf7b00028e69eb8f5bf7003e2
Jacob Carratello <jacobcar.22983@redlandsschools.net>
2018-09-12 15:25:58 +0000

    math

---
+++ carratello_pythagorean.py
@@ -0,0 +1,9 @@
+from math import sqrt
+name=input("Please enter your name.")
+print("Hello", name + ".")
+a=input("Lets do some pythagorean theorem. Please enter a value for a:")
+print("the value for a is now:", a)
+b=input("Please enter a value for b:")
+print("the value for b is now:", b)
+print("Lets calculate the hypotenuse")
+print float(c = sqrt(a*a + b*b))
