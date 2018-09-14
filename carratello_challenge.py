c0e200fe41d91dab01be0a49e502afb42f91f08c
Jacob Carratello <jacobcar.22983@redlandsschools.net>
2018-09-14 15:07:17 +0000

    math

--- carratello_challenge.py
+++ carratello_challenge.py
@@ -1,9 +1,9 @@
 from math import sqrt
 
 print("Enter the three legs of a triangle to see if it is a triangle, a right triangle, an obtuse triangle, and/or an acute triange.")
-side_a=input("Input side a:")
-side_b=input("Input side b:")
-side_c=input("Input side c:")
+side_a=int(input("Input side a:"))
+side_b=int(input("Input side b:"))
+side_c=int(input("Input side c:"))
 def is_triangle():
     if (side_a > side_b + side_c) or (side_b > side_a + side_c) or (side_c > side_a + side_b):
         print ("Not a triangle.")
@@ -11,3 +11,24 @@ def is_triangle():
         print ("Is a triangle.")
 
 is_triangle()
+def is_right_triangle():
+    if (side_a * side_a + side_b * side_b == side_c * side_c) or (side_b * side_b + side_c * side_c == side_a * side_a) or (side_c * side_c + side_a * side_a == side_b * side_b):
+        print("Is a right triangle")
+    else:
+        print("Not a right triangle")
+        
+is_right_triangle()
+def is_obtuse():
+    if (side_a * side_a + side_b * side_b < side_c * side_c) or (side_b * side_b + side_c * side_c < side_a * side_a) or (side_c * side_c + side_a * side_a < side_b * side_b):
+        print("Is obtuse")
+    else:
+        print("Not obtuse")
+        
+is_obtuse()
+def is_acute():
+    if (side_a * side_a + side_b * side_b > side_c * side_c) or (side_b * side_b + side_c * side_c > side_a * side_a) or (side_c * side_c + side_a * side_a > side_b * side_b):
+        print("Is acute")
+    else:
+        print("Not acute")
+        
+is_acute()
---
+++ evenOrOddPt2.py
@@ -0,0 +1,5 @@
+x=int(input("Enter an integer and see if it is even or odd:"))
+if x % 2 ==0:
+    print(x ,"is an even number")
+else:
+    print(x ,"is not an even number")
